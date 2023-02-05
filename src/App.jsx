e6ba6a6f4688bfed672f30a4096d483b96cf2fa9
Rosmar10 <94025464+Rosmar10@users.noreply.github.com>
2023-02-05 00:07:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { Routes, Route } from "react-router-dom";
+
+//pages 
 import Home from "./pages/Home";
 import Pizzas from "./pages/Pizzas";
 import Pizza from "./pages/Pizza";
 import Cart from "./pages/Cart";
+
+//Components
 import Navbar from "./components/Navbar";
 
+
+
 export default function App() {
+
   return (
     <div>
       <Navbar />
 
       <main className="container">
         <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/pizzas"
-            element={<Pizzas />}
-          />
-          <Route
-            path="/pizzas/:id"
-            element={<Pizza />}
-          />
-          <Route
-            path="/cart"
-            element={<Cart />}
-          />
-          {/* Agregar ruta 404 */}
+          <Route path="/" element={<Home />} />
+          <Route path="/pizzas" element={<Pizzas />} />
+          <Route path="/pizzas/:id" element={<Pizza />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<h1>404 page not found</h1>} />
         </Routes>
       </main>
     </div>
